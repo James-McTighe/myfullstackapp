a4941bd13d3cccec974538b8476193a8b41c60d3
James McTighe <james.mctighe97@gmail.com>
2026-05-07 11:20:44 -0400

    removed unused routes

--- server/src/home/routes.py
+++ server/src/home/routes.py
@@ -6,20 +6,8 @@ main = Blueprint("main", __name__)
 
 HOME_CONTENT_PATH = Path(__file__).with_name("home-content.json")
 
-
-@main.route("/")
-@main.route("/home")
-def home():
-    pass
-
-
 @main.route("/api/data", methods=["GET"])
 def get_data():
     with HOME_CONTENT_PATH.open("r", encoding="utf-8") as file:
         data = json.load(file)
     return jsonify(data)
-
-
-@main.route("/about")
-def about():
-    pass
