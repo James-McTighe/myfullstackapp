ec4a1e1dcba2230683f2dbae83f582cd87040108
James McTighe <james.mctighe97@gmail.com>
2026-05-07 11:10:48 -0400

    removed redundant column setting

--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -51,7 +51,7 @@ const Home = () => {
           {error}
         </div>
       ) : (
-        <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+        <div className="grid gap-6 lg:grid-cols-3">
           {cardData.map((card, index) => (
             <InfoCard 
               key={card.id || index}
