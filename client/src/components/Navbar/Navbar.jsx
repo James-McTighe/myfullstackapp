9c2ddf8f4fc2ee82d4d63810a03e7e39afec95fa
James McTighe <james.mctighe97@gmail.com>
2026-05-08 18:10:51 -0400

    added header to navbar and ran linter

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -3,19 +3,22 @@ import "./NavbarElements.css";
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <div className="navbar__menu">
-        <NavLink className="navbar__link" to="/">
-          Home
-        </NavLink>
-        <NavLink className="navbar__link" to="/job-apps">
-          Job Apps
-        </NavLink>
-        <NavLink className="navbar__link" to="/test-page">
-          Test Page
-        </NavLink>
-      </div>
-    </nav>
+    <>
+      <h1 className="p-5 pb-0.5 text-2xl text-emerald-500 bg-[#111827] ">James McTighe's Full Stack web app!</h1>
+      <nav className="navbar">
+        <div className="navbar__menu">
+          <NavLink className="navbar__link" to="/">
+            Home
+          </NavLink>
+          <NavLink className="navbar__link" to="/job-apps">
+            Job Apps
+          </NavLink>
+          <NavLink className="navbar__link" to="/test-page">
+            Test Page
+          </NavLink>
+        </div>
+      </nav>
+    </>
   );
 };
 
