cb50aa8146189ab01cb1c22df34f02bb6b0607db
James McTighe <james.mctighe97@gmail.com>
2026-05-08 02:47:58 -0400

    made it so snackbar can have a message passed into it

--- client/src/components/utils/Toast.jsx
+++ client/src/components/utils/Toast.jsx
@@ -4,7 +4,7 @@ import Snackbar from '@mui/material/Snackbar';
 import IconButton from '@mui/material/IconButton';
 import CloseIcon from '@mui/icons-material/Close';
 
-export default function SimpleSnackbar() {
+export default function SimpleSnackbar({ message }) {
   const [open, setOpen] = React.useState(false);
 
   const handleClick = () => {
@@ -39,7 +39,7 @@ export default function SimpleSnackbar() {
         open={open}
         autoHideDuration={2000}
         onClose={handleClose}
-        message="Note archived"
+        message={message}
         action={action}
       />
     </div>
